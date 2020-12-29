3434edcd2b3e0e28c0f52019c6b84fbf89893261
brieuc-mac <brieuc.lehmann@bdi.ox.ac.uk>
2020-12-29 15:04:38 +0000

    Fix table headings in summary

--- R/summary.multibergm.R
+++ R/summary.multibergm.R
@@ -21,67 +21,67 @@ summary.multibergm <- function(object,
                                ...) {
   thin    <- as.integer(thin)
   burn_in <- as.integer(burn_in)
-
+  
   # Remove burn_in and apply thinning
   post_iters      <- seq(burn_in + 1L, object$main_iters, thin)
   output  <- get(param, object$params)
   output  <- subset(output, iterations = post_iters)
-
+  
   object$accepts$theta <- object$accepts$theta[post_iters, ,drop=FALSE]
   object$accepts$mu <- object$accepts$mu[post_iters, ,drop=FALSE]
   model_terms <- object$control$model$coef.names
   n_terms     <- length(attr(terms(object$formula), "term.labels"))
-
+  
   cat("\n", "Posterior Density Estimate for Model: \ny ~",
       paste(object$formula[3]), "\n", "\n")
-
+  
   if (param == "mu_pop") {
     ff_mu   <- mcmcr::as.mcmc(output, start = burn_in + 1, thin = thin)
-
+    
     table1 <- summary(ff_mu)$statistics
     rnames <- paste0("mu", seq_len(n_terms), " (", model_terms, ")")
     table1 <- matrix(table1, n_terms,
-                     dimnames = list(rnames, names(table1)))
-
+                     dimnames = list(rnames, colnames(table1)))
+    
     table2 <- summary(ff_mu)$quantiles
     table2 <- matrix(table2, n_terms,
-                     dimnames = list(rnames, names(table2)))
-
+                     dimnames = list(rnames, colnames(table2)))
+    
     print(table1, digits = 4)
     cat("\n")
     print(table2, digits = 4)
-
+    
   } else {
     for (g in 1:object$init$n_groups) {
       ff_mu   <- coda::mcmc(output, start = burn_in + 1, thin = thin)
-
+      
       table1 <- summary(ff_mu)$statistics
       rnames <- paste0("mu", seq_len(n_terms), " (", model_terms, ", G",
                        rep(g, each = n_terms), ")")
       table1 <- matrix(table1, n_terms,
                        dimnames = list(rnames, colnames(table1)))
-
+      
       table2 <- summary(ff_mu)$quantiles
       table2 <- matrix(table2, n_terms,
                        dimnames = list(rnames, colnames(table2)))
-
+      
       print(table1, digits = 4)
       print("\n")
       print(table2, digits = 4)
     }
   }
-
+  
   # Print acceptance rates
   cat("\n Theta acceptance rate: \n")
   theta_ar <- colSums(object$accepts$theta) / (length(post_iters) - 1)
   cat(formatC(mean(theta_ar), digits = 3, format = "f"),
       " (", formatC(min(theta_ar), digits = 3, format = "f"), ", ",
       formatC(max(theta_ar), digits = 3, format = "f"), ")", sep = "")
-
+  
   cat("\n Mu acceptance rate: \n")
   mu_ar <- colSums(object$accepts$mu) / (length(post_iters) - 1)
   cat(formatC(mean(mu_ar), digits = 3, format = "f"),
       " (", formatC(min(mu_ar), digits = 3, format = "f"), ", ",
       formatC(max(mu_ar), digits = 3, format = "f"), ") \n", sep = "")
-
+  
 }
